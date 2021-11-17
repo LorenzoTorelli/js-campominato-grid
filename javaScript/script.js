fcc8b1848f5bf7b503b8e1da18867dfacef14a57
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-11-17 17:19:48 +0100

    griglia ultimata

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,7 +1,7 @@
 
 let container = document.getElementById("container-grid");
 let play = document.getElementById('play');
-
+let bombe = [];
 
 // svuota la griglia
 function svuotaGriglia() {
@@ -15,28 +15,54 @@ function addSquare() {
     return elem;
 }
 
-// creazione griglia 
-function creagriglia(livello) {
 
+// creazione griglia 
+function Gioca(livello, bombe) {
+    let game = true;
     for (let i = 0; i< livello; i++) {
         const divElem = addSquare();
         divElem.classList.add('square-' + livello);
         divElem.innerHTML= i+1;
         container.appendChild(divElem);
-
-        // divElem.addEventListener("click", control(divElem));
+        changeColor(divElem, bombe, game);
+        console.log(game);
+        if (game == false) {
+            break;
+        }
     }
-
 }
-// function control(divElem) {
-//     if (divElem.classList.contains('click-true')) {
-//         divElem.classList.add()
-//     };    
+
+// crea array con caselle con bombe
+function creaBombe(livello) {
+    let bombe = [];
+    for (let i = 0; i<Math.sqrt(livello); i++) {
+        let a = Math.floor(Math.random() * (livello - 1) + 1);
+        bombe.push(a);
+    }
+    return bombe;
+}
+
+// cambia colore al click 
+function changeColor(divElem, bombe, game) {
+    divElem.addEventListener("click", function () {
+        if (bombe.includes(parseInt(divElem.textContent))) {
+            divElem.classList.add("click-false");
+            game = false;
+            
+        }
+        else{
+            divElem.classList.add("click-true");
+        }
+    });
+}
+
+
 
 play.addEventListener("click", function() {
     let select = document.getElementById('livello');
     let livello = select.options[select.selectedIndex].value;
-    console.log(livello);
+    let bombe = creaBombe(livello) 
+    console.log(bombe);
     svuotaGriglia();
-    creagriglia(livello);
+    Gioca(livello, bombe);
 })
